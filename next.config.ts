353fecbb16c7987f46a2328ef7c7dc3b2c0b8bd7
charles2006hk-hash <charles2006hk@gmail.com>
2025-12-27 20:58:49 +0800

    Disable experimental reactCompiler in next.config.ts
    Removed experimental reactCompiler option to use stable compiler.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactCompiler: true,
+  // 確保沒有啟用 experimental: { reactCompiler: true }
+  // 這樣 Next.js 就會使用預設的穩定編譯器
 };
 
 export default nextConfig;
