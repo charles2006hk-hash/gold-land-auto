7fc55443ec5b34fba79460a35fd6db1221ced5ab
charles2006hk-hash <charles2006hk@gmail.com>
2026-05-07 15:29:26 +0800

    Add updateSubItem prop to VehicleFormModal

--- src/components/VehicleFormModal.tsx
+++ src/components/VehicleFormModal.tsx
@@ -68,7 +68,7 @@ const VehicleFormModal = ({
     db, staffId, appId, clients, settings, editingVehicle, setEditingVehicle, activeTab, setActiveTab, saveVehicle, addPayment, deletePayment, addExpense, deleteExpense,
     updateExpenseStatus, addSystemLog, allSalesDocs, onJumpToDoc,
     addSalesAddon = () => {}, deleteSalesAddon = () => {},
-    updateSettings, systemUsers, currentUser
+    updateSettings, systemUsers, currentUser, updateSubItem
 }: any) => {
     if (!editingVehicle && activeTab !== 'inventory_add') return null; 
     
