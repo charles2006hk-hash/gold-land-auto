d43e8b831b689968947eabe49dd33799e3bc7879
charles2006hk-hash <charles2006hk@gmail.com>
2026-09-07 15:11:58 +0800

    Change VehicleFormModal to use min-h-0 class

--- src/components/VehicleFormModal.tsx
+++ src/components/VehicleFormModal.tsx
@@ -1612,7 +1612,7 @@ const VehicleFormModal = ({
                 </div>
 
                 {/* 分頁內容區 */}
-                <div className="flex-1 overflow-y-auto p-4 md:p-6 bg-white scrollbar-thin relative w-full overflow-x-hidden">
+                <div className="flex-1 min-h-0 overflow-y-auto p-4 md:p-6 bg-white scrollbar-thin relative w-full overflow-x-hidden">
                     
                     {/* ===== Tab 1: 銷售與收款 (Sales) ===== */}
                     <div className={`${rightTab === 'sales' ? 'block' : 'hidden'} md:${rightTab === 'vrd' || rightTab === 'sales' ? 'block' : 'hidden'} space-y-6 animate-fade-in w-full`}>
